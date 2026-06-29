d7e3b2d74ab2b719d5310ebe7740ad7db20b3649
Amit Khatri <aamita96@gmail.com>
2026-06-29 22:33:06 +0530

    Refactor App component to use Suspense for lazy loading screens and improve loading state handling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,32 @@
 import './App.css'
 import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header'
-import LandingPage from './screens/LandingPage/LandingPage'
+import Loading from './components/Loading/Loading';
+
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import MyNotes from './screens/MyNotes/MyNotes';
-import LoginPage from './screens/LoginPage/LoginPage';
-import RegisterPage from './screens/RegisterPage/RegisterPage';
+import { lazy, Suspense } from 'react';
+
+
 
 function App() {
+
+  const RegisterPage = lazy(() => import('./screens/RegisterPage/RegisterPage'));
+  const LandingPage = lazy(() => import('./screens/LandingPage/LandingPage'));
+  const LoginPage = lazy(() => import('./screens/LoginPage/LoginPage'));
+  const MyNotes = lazy(() => import('./screens/MyNotes/MyNotes'));
 
   return (
     <BrowserRouter>
       <Header />
       <main>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<RegisterPage />} />
-          <Route path="/mynotes" element={<MyNotes />} />
-        </Routes>
+        <Suspense fallback={<Loading />}>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={<RegisterPage />} />
+            <Route path="/mynotes" element={<MyNotes />} />
+          </Routes>
+        </Suspense>
       </main>
       <Footer />
     </BrowserRouter>
